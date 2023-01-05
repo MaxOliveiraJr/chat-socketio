663cd57c1aca5422ed05305f6125b340883e6ba8
Max Oliveira <max-oliveira@outlook.com.br>
2023-01-04 22:03:57 -0300

    feat: socket io configurado

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const app = express();
+const socketIO = require('socket.io');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.listen(3000,()=>{
+const server = app.listen(3000, () => {
     console.log('Runnig port 3000')
+})
+
+
+// const server = http.Server(app);
+
+// server.listen(3000, () => {
+//     console.log('Running port 3000')
+// })
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log("New Connection")
 })
